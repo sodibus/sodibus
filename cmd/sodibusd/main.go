6f3ffbc923fbff0b36517f40a4a82efd84a6e169
Ryan Wade <ryan.wade@protonmail.com>
2017-04-09 01:28:36 +0800

    Add cmd/sodibusd, add protobufs

---
+++ cmd/sodibusd/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+import _ "github.com/sodibus/sodibus"
+
+func main() {
+	fmt.Println("hello world!")
+}
---
+++ server.go
@@ -0,0 +1,3 @@
+package sodibus
+
+import _ "github.com/sodibus/sodibus/packet"
